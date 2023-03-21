e76baa6bdbe440168bab8b76d09baf9f46a643c4
Halit Uzan <halit.uzan@gmail.com>
2023-03-21 20:44:24 +0300

    update

--- src/features/sozlukAction.js
+++ src/features/sozlukAction.js
@@ -11,21 +11,19 @@ export function fetchTdkSearching(searching) {
   return async (dispatch) => {
     dispatch(getTdk());
     try {
-      const data  = await tdkAllApi(searching,"/");
+      const data = await tdkAllApi(searching, "/api/");
       dispatch(getTdkSuccess(data));
     } catch (error) {
       dispatch(getTdkFailure());
     }
   };
 }
-
+console.log(tdkAllApi);
 export function fetchTdk(num) {
   return async (dispatch) => {
     dispatch(getTdk());
     try {
-      const { data } = await axios.get(
-        `/gts_id?id=${num}`
-      );
+      const { data } = await axios.get(`/gts_id?id=${num}`);
       dispatch(getTdkSuccess(data[0]));
     } catch (error) {
       dispatch(getTdkFailure());
@@ -36,9 +34,7 @@ export function fetchTdk(num) {
 export function fetchTdkFav(num) {
   return async (dispatch) => {
     try {
-      const { data } = await axios.get(
-        `/gts_id?id=${num}`
-      );
+      const { data } = await axios.get(`/gts_id?id=${num}`);
       dispatch(getFavWord(data));
     } catch (error) {
       console.log(error);
